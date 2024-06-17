fc6e3dba62d55b771ac9536b41e38464b6a5b26f
princejustice1 <pjakpaloo@st.ug.edu.gh>
2024-06-17 17:32:42 +0000

    Inheritance and Method overriding finished

---
+++ Inheritance_methodoverriding/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Animal
+{
+
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Animal firstAnimal = new Animal();
+        Dog firstDog = new Dog();
+        Cat firstCat = new Cat();
+
+        firstAnimal.MakeSound();
+        firstDog.MakeSound();
+        firstCat.MakeSound();
+    }
+}
